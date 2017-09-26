b6350b155cabcebed7b72a4099d50b1149fedebd
TheFonix <actualfriction@gmail.com>
2017-09-26 11:38:03 +0100

    Massive Re-write and Break Fixes

--- flo.sh
+++ flo.sh
@@ -4,55 +4,63 @@ cd /mnt/server
 #Create the server information so the pre-flight picks up on it
 curl https://git.corinthmc.com/snippets/8/raw > server.properties
 
-echo #!/bin/bash
+echo '#!/bin/bash
 
 #Variables
 SERVERLOC="/home/container"
 
 #CorinthMC Flo Starter
-echo "╭━━━┳╮    "
-echo "┃╭━━┫┃    "
-echo "┃╰━━┫┃╭━━╮"
-echo "┃╭━━┫┃┃╭╮┃"
-echo "┃┃╱╱┃╰┫╰╯┃"
-echo "╰╯╱╱╰━┻━━╯"
+echo " ______  ___      _______  "
+echo "|       ||   |    |       |"
+echo "|    ___||   |    |   _   |"
+echo "|   |___ |   |    |  | |  |"
+echo "|    ___||   |___ |  |_|  |"
+echo "|   |    |       ||       |"
+echo "|___|    |_______||_______|"
 
 echo "Created By Fonix Hardcastle"
 echo "  https://fonix.online"
+echo "          v4.2"
 
 #Pause for a few seconds!
   sleep 2
 
 #Move and Empty the Directory & Refil from the Git into $SERVERLOC
-echo "Checking if the Directory is empty"
+echo -ne '#                         (0%)\r'
+sleep 1
   if [ "$(ls -A $SERVERLOC)" ];
 
     then
-      echo "Found that the directory isnt empty, Emptying now!"
+      echo -ne '##                        (5%)\r'
       sleep 1
-      mv start.sh /tmp & echo "Moving the start script to /tmp  1/3"
-      mv server.properties /tmp & echo "Moving the server.properties file to /tmp   2/3"
+      mv start.sh /tmp & echo -ne '###                       (15%)\r'
+      sleep 1
+      mv server.properties /tmp & echo -ne '####                      (24%)\r'
       sleep 1
       rm -rf $SERVERLOC/* 2> /dev/null
-      echo "Clear the contents of $SERVERLOC   3/3"
+      echo -ne '######                    (31%)\r'
+      sleep 1
       rm -rf $SERVERLOC/.* 2> /dev/null
-      echo "Clean Up Complete Moving to Server pull Task"
+      echo -ne '########                  (43%)\r'
       sleep 1
 
     else
-      echo "This is the first time the server is being ran? Awesome! HI!"
-      sleep 2
   fi
 
 cd $SERVERLOC
- echo "Pulling the Container from https://git.Corinthmc.com"
-  git clone https://*:*@git.*.com:443/Corinth/container.git . >/dev/null 2>&1
-    echo "Pull Complete!"
+ echo -ne '##########                (58%)\r'
+ sleep 1
+  git clone https://*:*@git.*.com:443/Corinth/container.git . 2> /dev/null
+    echo -ne '##################        (75%)\r'
+    sleep 1
 
-echo "Moving files back in the required places."
+echo -ne '########################  (94%)\r'
+sleep 1
   mv /tmp/start.sh . > /dev/null
     mv /tmp/server.properties . > /dev/null
 
+echo -ne '##########################(100%)\r'
+sleep 1
 echo "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-"
 echo "----> All Tasks have been complete! Starting Server <----"
 echo "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-"
