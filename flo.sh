7f4f59891287aa5b7aa52184b34ad0a3d063e260
Fonix Hardcastle <me@fonix.online>
2017-10-07 17:50:26 +0100

    Add CPU usage variable

--- flo.sh
+++ flo.sh
@@ -8,44 +8,45 @@ echo '#!/bin/bash
 
 #Variables
 SERVERLOC="/home/container"
+TMPLOC="/tmp"
 
-#CorinthMC Flo Starter
-echo " _______  ___      _______  "
-echo "|       ||   |    |       |"
-echo "|    ___||   |    |   _   |"
-echo "|   |___ |   |    |  | |  |"
-echo "|    ___||   |___ |  |_|  |"
-echo "|   |    |       ||       |"
-echo "|___|    |_______||_______|"
+#Locate to the Servers location ($SERVERLOC)
+cd $SERVERLOC
 
-echo "Created By Fonix Hardcastle"
-echo "  https://fonix.online"
-echo "          v4.2"
-
-#Pause for a few seconds!
-  sleep 2
-
-#Move and Empty the Directory & Refil from the Git into $SERVERLOC
+#If Server is empty check
   if [ "$(ls -A $SERVERLOC)" ];
     then
+      #Make sure the /tmp directory exists
+        mkdir -p $TMPLOC
+        
+        #Copy the start.sh + logs + server.properties to the /tmp directory
+          cp $SERVERLOC/start.sh $TMPLOC
+            cp $SERVERLOC/server.properties $TMPLOC
+              cp -r $SERVERLOC/logs $TMPLOC/logs
+
+      #Remove everything from the container! (Apart from /tmp)        
       rm -rf $SERVERLOC/* 2> /dev/null
       rm -rf $SERVERLOC/.* 2> /dev/null
     else
+      
+      #First time the server has been ran, resync not needed!
       echo "Welcome first time user!"
   fi
+  
+  #Build the CPU and Ram Variables for the system!
+  echo 'INTERNAL_CPU_USAGE="grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage}'"' >> /etc/profile
+  
+  #Git pulling (Without Debug info, so we dont spam the console)
+  git clone --quiet https://*@*git.corinthmc.com:443/Corinth/Flo/flo-1.9.git . 2> /dev/null
+  
+    #Move the start.sh + server.properties + the logs back to the server
+    cp $TMPLOC/start.sh $SERVERLOC/
+      cp $TMPLOC/server.properties $SERVERLOC/
+        cp -r $TMPLOC/logs $SERVERLOC/
 
-cd $SERVERLOC
-  git clone --quiet https://*:*@git.corinthmc.com:443/Corinth/Flo/flo-1.9.git .
+        #Delete anything older than 5 days from the container
+         find $TMPLOC* -mtime +5 -exec rm {} \;
 
-sleep 1
-  mkdir -p /tmp
-    mv /tmp/start.sh /home/container/ 2> /dev/null
-      mv /tmp/server.properties /home/container/ 2> /dev/null
-
-echo "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-"
-echo "----> All Tasks have been complete! Starting Server <----"
-echo "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-"
-sleep 2
 
 clear' > start.sh
 
